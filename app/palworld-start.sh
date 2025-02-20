cfe810bbc76f78c5acc364a01532f00949cb4b31
ParkSnoopy <sunwoo2539@gmail.com>
2025-02-20 10:54:48 +0000

    update: Always update game to latest on startup

--- app/palworld-start.sh
+++ app/palworld-start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# update server
+DIR=$( dirname "$(realpath $0)" )
+$DIR/palworld-update.sh
+
 /home/steam/Steam/steamapps/common/PalServer/PalServer.sh --userperfthreads --NoAsyncLoadingThread --UseMultithreadForDS
